bc6d9597de574ba5f0d8021cb3214ed53e7e9085
bonmarkzz <bonmark.son@gmail.com>
2024-06-03 16:43:15 -0700

    Update gui.js

--- cheats/gui.js
+++ cheats/gui.js
@@ -1905,11 +1905,11 @@
                                 class: "🤖",
                                 rarity: "Legendary",
                                 cash: [8e4, 43e4, 42e5, 62e6, 1e9],
-                                time: [0.01, 0.01, 0.01, 0.01, 0.01],
+                                time: [0.001, 0.001, 0.001, 0.001, 0.001],
                                 price: [7e6, 12e7, 19e8, 35e9],
                                 active: false,
                                 level: 4,
-                                bonus: 5.5
+                                bonus: 20
                             }))
                         });
                     }
